c4de846d9c30bed20e66f7edfc32e3b644b3f4b2
Attinderpal <singh.at@husky.neu.edu>
2018-06-19 22:56:51 -0400

    server v

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(session({
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        "https://nodejs-client-assignment5.herokuapp.com'");
+        "https://nodejs-client-assignment5.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
